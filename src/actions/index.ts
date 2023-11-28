28660b76edd11b5a68aab527744d54635709ec0f
Trabontea <danielmbotea@gmail.com>
2023-11-29 01:21:07 +0200

    error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,63 +1,75 @@
 'use server';
-import {db} from '@/db';
-import {redirect} from 'next/navigation'
+import { db } from '@/db';
+import { redirect } from 'next/navigation';
 
-
-//functia asta importata si apelata 
+//functia asta importata si apelata
 //in componenta client SnippetEditForm.tsx
 
-export async function editSnippet(id:number, code: string) {
+export async function editSnippet(id: number, code: string) {
   // se vede in consolea IDE
-  // console.log(id, code) 
+  // console.log(id, code)
 
   await db.snippet.update({
-    where: {id},
-    data: {code}
+    where: { id },
+    data: { code },
   });
 
-  redirect(`/snippets/${id}`)
+  redirect(`/snippets/${id}`);
 }
 
-export async function deleteSnippet(id:number) {
+export async function deleteSnippet(id: number) {
   await db.snippet.delete({
-    where: {id}
-  })
+    where: { id },
+  });
 
-  redirect('/')
+  redirect('/');
 }
 
-
-export async function createSnippet(formState: {message: string},  formData: FormData) {
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
   // This needs to be a server action!
   // 'use server';
 
+  try {
+    // Check the user's inputs and make sure they'are valid
+    const title = formData.get('title');
+    const code = formData.get('code');
 
-  // Check the user's inputs and make sure they'are valid 
-  const title  =  formData.get('title')
-  const code  =  formData.get('code') 
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer',
+      };
+    }
 
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: 'Title must be longer'
+    if (typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be longer',
+      };
     }
-  } 
-  
-  if(typeof code !== 'string' || code.length < 10 ) {
-    return {
-      message: 'Code must be longer'
+
+    // Create a new recolrd in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: 'Something went wrong',
+      };
     }
+
+    throw new Error("We can't record!");
   }
-
-  // Create a new recolrd in the database
-
-  const snippet = await db.snippet.create({
-    data: {
-      title,
-      code
-    }
-  });
-  console.log('Snippet', snippet)
 
   // Redirect the user back to the root route
   redirect('/');
-};
+}
